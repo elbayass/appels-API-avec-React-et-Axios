18e2b1e576ce778757068ad796f74dd0ea842295
elbayass <elbayass59@gmail.com>
2019-02-08 09:15:38 +0100

    recup info api

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-class App extends Component {
+export default class PersonList extends React.Component {
+  state = {
+    persons: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://api.github.com/users`)
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+      })
+  }
+
+
   render() {
+
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container_user">
+        { this.state.persons.map(person => 
+          <div className="container_cart">
+            <img src={person.avatar_url} alt="profile" className="profile" />
+              <div className='info_cart'>
+                <h3>{person.login}</h3>
+                <h6> followers : {person.followers_url}</h6>
+                <h6> public repos : {person.repos_url}</h6>
+              </div >
+          </div>
+        )}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+
